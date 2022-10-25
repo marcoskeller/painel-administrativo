0c5d60f33b34c0a83ab8915050df7d6a449fb849
Marcos Keller <marcoskeller@hotmail.com>
2022-10-25 19:57:01 -0300

    Implementacao de toda estrutura do projeto

---
+++ Painel_Administrativo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Painel_Administrativo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
